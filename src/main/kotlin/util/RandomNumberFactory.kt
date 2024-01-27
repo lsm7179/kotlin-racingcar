26eb23ff324423f52e53ed02a7bd8baf668e989e
이승민 <sml7179@naver.com>
2024-01-27 10:27:00 +0900

    refactor: 싱글톤 팩토리로 변경

--- src/main/kotlin/util/RandomNumberFactory.kt
+++ src/main/kotlin/util/RandomNumberFactory.kt
@@ -1,12 +1,12 @@
 package util
 
-class RandomNumberFactory {
-    companion object {
-        private const val FROM_NUMBER: Int = 0
-        private const val UNTIL_NUMBER: Int = 9
-        fun create(): Int {
-            return (FROM_NUMBER..UNTIL_NUMBER).random()
-        }
+object RandomNumberFactory {
+
+    private const val FROM_NUMBER: Int = 0
+    private const val UNTIL_NUMBER: Int = 9
+    fun create(): Int {
+        return (FROM_NUMBER..UNTIL_NUMBER).random()
     }
+
 
 }
